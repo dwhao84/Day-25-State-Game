c0ba334c6079f5ee85c7049d82ba9f7a402042cb
Dawei Hao <dwsamurai84@gmail.com>
2024-10-18 04:24:43 +0800

    add while loop for checking len(guessed_states)

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ def get_mouse_click_cor(x, y):
 def add_state(letter, x, y):
     t.penup()
     t.write(letter, align="center", font=FONT)
-    
-# Set a score as variable
-score = 0
-
-is_finish = True
 
 screen = t.Screen()
 screen.title("U.S. State Game")
@@ -26,27 +21,31 @@ t.shape(image)
 data = pandas.read_csv("50_states.csv")
 states = data["state"]  # 找到所有的state。
 print(states)           # 印出所有的state。
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name")
-for answer in states:
-    if answer == answer_state:
-        score += 1
-        print(f"Correct Answer { answer_state }")
-        # 找到相同state，取出state那一欄的數值。
-        state_data = data[data.state == answer_state].iloc[0]
-        # 透過state_data.x找到x_loc, y_loc。
-        x_loc, y_loc = state_data.x, state_data.y
-        # 加上州的資料。
-        add_state(answer_state, x_loc, y_loc)
-    else:
-        print("you're enter the wrong answer.")
-        
-print(score) # 計算分數
-print(f"{ answer_state }")
-screen.title(f"{ score } / 50 States Correct")
-
-# t.onscreenclick(get_mouse_click_cor)
-t.mainloop()
+# 可以用append的方式，找到數字。
+while len(guessed_states) < 50:
+    # Add title()，可以用在確認不管是否能用在辨識大小寫。
+    answer_state = screen.textinput(title=f"{len(guessed_states)} / 50 Guess the State",
+                                    prompt="What's another state's name").title()
+    for answer in states:
+        if answer == answer_state:
+            guessed_states.append(answer)
+            print(f"Correct Answer { answer_state }")
+            # 找到相同state，取出state那一欄的數值。
+            state_data = data[data.state == answer_state].iloc[0]
+            # 透過state_data.x找到x_loc, y_loc。
+            x_loc, y_loc = state_data.x, state_data.y
+            # 加上州的資料。
+            add_state(answer_state, x_loc, y_loc)
+        else:
+            print("you're enter the wrong answer.")
+            
+    print(f"{ answer_state }")
+    
+    # t.onscreenclick(get_mouse_click_cor)
+    t.mainloop()
+    
 screen.exitonclick()
 
 """
